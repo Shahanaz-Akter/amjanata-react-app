b1d9816fd849949eb2ff45a8ded77d068349cbdb
Shahanaz_Akter <shahanazakterm1999@gmail.com>
2024-04-27 17:56:51 +0600

    27_april

--- src/components/product/cart.jsx
+++ src/components/product/cart.jsx
@@ -1,11 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function Cart() {
-    return (
+  const [count, setCount] = useState(0);
+
+const handleClick=()=>{
+  setCount(count+1);
+
+}
+
+  return (
     <div className="page-content" style={{marginBottom: '100px'}}>
-         <div>Cart page</div>
+      <div className='cart'>Cart page</div>
+       
+       
+         <div onClick={handleClick} id='click'> Click pls</div>
+
+      <div>
+              Clicked {count} times
+      </div>
     </div>
-    );
+  );
 }
 
 export default Cart;
